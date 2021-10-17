777320c9d22f37d7fc2a76f58b8be435615fdb3e
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-10-17 17:07:20 +0530

    Add files via upload

---
+++ JS/Swiper.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    slidesPerView: 3,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      mousewheel: true,
+      keyboard: true,
+      autoplay: true
+  });
+
+
+  var mq = window.matchMedia( "(max-width: 992px)" );
+  if (mq.matches) {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        slidesPerView: 2,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          mousewheel: true,
+          keyboard: true,
+          autoplay: true
+      });
+   }
